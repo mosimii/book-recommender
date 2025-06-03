4f69fae09defbe6422b6155c4472c9abddf77c15
mosimii <simisogunle@gmail.com>
2025-06-03 20:54:03 +0100

    Save the data as a csv file

---
+++ gradio-dashboard.py
@@ -0,0 +1,118 @@
+# import required libraries
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
+
+from langchain_community.document_loaders import TextLoader
+from langchain_openai import OpenAIEmbeddings
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_chroma import Chroma
+
+import gradio as gr
+
+load_dotenv() # load environment variables
+
+# load book data and prepare image URLs
+books = pd.read_csv("books_with_emotions.csv")
+books["large_thumbnail"] = books["thumbnail"] + "&fife=w800"  # generate large image URLs
+books["large_thumbnail"] = np.where(
+    books["large_thumbnail"].isna(),
+    "cover-not-found.jpg", # using placeholder if missing
+    books["large_thumbnail"],
+)
+
+# load and split raw text for semantic search
+raw_documents = TextLoader("tagged_description.txt").load()
+text_splitter = CharacterTextSplitter(separator="\n", chunk_size=0, chunk_overlap=0)
+documents = text_splitter.split_documents(raw_documents)
+db_books = Chroma.from_documents(documents, OpenAIEmbeddings()) # create semantic search database using OpenAI embeddings
+
+# search for similar books based on a query and optional filters
+def retrieve_semantic_recommendations(
+        query: str,
+        category: str = None,
+        tone: str = None,
+        initial_top_k: int = 50,
+        final_top_k: int = 16,
+) -> pd.DataFrame:
+
+    # get top similar books from the semantic search index
+    recs = db_books.similarity_search(query, k=initial_top_k)
+    books_list = [int(rec.page_content.strip('"').split()[0]) for rec in recs]
+    book_recs = books[books["isbn13"].isin(books_list)].head(initial_top_k)
+
+    # filter by category if specified
+    if category != "All":
+        book_recs = book_recs[book_recs["simple_categories"] == category].head(final_top_k)
+    else:
+        book_recs = book_recs.head(final_top_k)
+
+    # sort by emotional tone
+    if tone == "Happy":
+        book_recs.sort_values(by="joy", ascending=False, inplace=True)
+    elif tone == "Surprising":
+        book_recs.sort_values(by="surprise", ascending=False, inplace=True)
+    elif tone == "Angry":
+        book_recs.sort_values(by="anger", ascending=False, inplace=True)
+    elif tone == "Suspenseful":
+        book_recs.sort_values(by="fear", ascending=False, inplace=True)
+    elif tone == "Sad":
+        book_recs.sort_values(by="sadness", ascending=False, inplace=True)
+
+    return book_recs
+
+# format recommendations for display
+def recommend_books(
+        query: str,
+        category: str,
+        tone: str
+):
+    recommendations = retrieve_semantic_recommendations(query, category, tone)
+    results = []
+
+    for _, row in recommendations.iterrows():
+        # truncate long descriptions for display
+        description = row["description"]
+        truncated_desc_split = description.split()
+        truncated_description = " ".join(truncated_desc_split[:30]) + "..."
+
+        # format author names
+        authors_split = row["authors"].split(";")
+        if len(authors_split) == 2:
+            authors_str = f"{authors_split[0]} and {authors_split[1]}"
+        elif len(authors_split) > 2:
+            authors_str = f"{', '.join(authors_split[:-1])}, and {authors_split[-1]}"
+        else:
+            authors_str = row["authors"]
+
+        caption = f"{row['title']} by {authors_str}: {truncated_description}"
+        results.append((row["large_thumbnail"], caption))
+    return results
+
+# set up dropdown options
+categories = ["All"] + sorted(books["simple_categories"].unique())
+tones = ["All"] + ["Happy", "Surprising", "Angry", "Suspenseful", "Sad"]
+
+# building the Gradio interface
+with gr.Blocks(theme = gr.themes.Glass()) as dashboard:
+    gr.Markdown("# Semantic book recommender")
+
+    with gr.Row():
+        # input fields
+        user_query = gr.Textbox(label = "Please enter a description of a book:",
+                                placeholder = "e.g., A story about forgiveness")
+        category_dropdown = gr.Dropdown(choices = categories, label = "Select a category:", value = "All")
+        tone_dropdown = gr.Dropdown(choices = tones, label = "Select an emotional tone:", value = "All")
+        submit_button = gr.Button("Find recommendations")
+
+    # output section
+    gr.Markdown("## Recommendations")
+    output = gr.Gallery(label = "Recommended books", columns = 8, rows = 2)
+
+    submit_button.click(fn = recommend_books,
+                        inputs = [user_query, category_dropdown, tone_dropdown],
+                        outputs = output)
+
+# launching the app
+if __name__ == "__main__":
+    dashboard.launch(share=True)
